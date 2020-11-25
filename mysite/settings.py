fc287a34e6af35e3836b76c586a89573d0f4a385
Adrian Kokai <adriankokai@gmail.com>
2020-11-25 11:02:01 +0200

    rewrote chat server as Asynchronous

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = '/static/'
 # mysite/settings.py
 # Channels
 ASGI_APPLICATION = 'mysite.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
